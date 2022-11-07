28dc60993750548a16a796bb97496bfdc78a94ab
joao-per <rodriguespereirajoaopaulo@gmail.com>
2022-11-07 10:44:10 +0000

    functional

---
+++ AVALIAR/get_next_line_bonus.c
@@ -0,0 +1,53 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   get_next_line_bonus.c                              :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-per <joao-per@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 12:31:46 by joao-per          #+#    #+#             */
+/*   Updated: 2022/11/04 00:59:32 by joao-per         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line_bonus.h"
+#include <unistd.h>
+
+char	*ft_read_to_left_str(int fd, char *left_str)
+{
+	char	*buff;
+	int		rd_bytes;
+
+	buff = malloc((BUFFER_SIZE + 1) * sizeof(char));
+	if (!buff)
+		return (NULL);
+	rd_bytes = 1;
+	while (!ft_strchr(left_str, '\n') && rd_bytes != 0)
+	{
+		rd_bytes = read(fd, buff, BUFFER_SIZE);
+		if (rd_bytes == -1)
+		{
+			free(buff);
+			return (NULL);
+		}
+		buff[rd_bytes] = '\0';
+		left_str = ft_strjoin(left_str, buff);
+	}
+	free(buff);
+	return (left_str);
+}
+
+char	*get_next_line(int fd)
+{
+	char		*line;
+	static char	*left_str[4096];
+
+	if (fd < 0 || BUFFER_SIZE <= 0)
+		return (0);
+	left_str[fd] = ft_read_to_left_str(fd, left_str[fd]);
+	if (!left_str[fd])
+		return (NULL);
+	line = ft_get_line(left_str[fd]);
+	left_str[fd] = ft_new_left_str(left_str[fd]);
+	return (line);
+}
---
+++ AVALIAR/tester.c
@@ -0,0 +1,81 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   tester.c                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: joao-per <joao-per@student.42lisboa.com>   +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/11/03 13:09:34 by joao-per          #+#    #+#             */
+/*   Updated: 2022/11/03 19:05:51 by joao-per         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "get_next_line.h"
+#include <unistd.h>
+#include <stdio.h>
+#include <fcntl.h>
+
+int	main(void)
+{
+	char	*line;
+	int	i;
+	int	text1;
+	int	text2;
+	int	text3;
+
+	text1 = open("test/test.txt", O_RDONLY);
+	text2 = open("test/test2.txt", O_RDONLY);
+	text3 = open("test/test3.txt", O_RDONLY);
+	i = 1;
+	
+	printf("|||||||||||||||||||||\n");
+	printf("IF YOU WANT TO TEST BONUS\nDECREMENT -1 IN EACH WHILE IN ORDER TO TEST MULTIPLE FILES\nOUTPUT SHALL NOT OVERLAP DIFFERENT TEXTS.\n");
+	printf("(Press Enter to Start)\n");
+	printf("|||||||||||||||||||||\n");
+	scanf("pause");
+	printf("||Test Number 1: Chorus||\n");
+	while (i < 7)
+	{
+		line = get_next_line(text1);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(text1);
+	printf("\n\n||Test Number 2: Stanzas||\n");
+	i = 1;
+	while (i < 10)
+	{
+		line = get_next_line(text2);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(text2);
+	printf("\n\n||Test Number 3: Null lines and spaces||\n");
+	i = 1;
+	while (i < 11)
+	{
+		line = get_next_line(text3);
+		if (!line)
+			printf("\n");
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(text3);
+	
+/* 	int text4;
+	printf("\n\n||Test Number 4: Full Song||\n");
+	text4 = open("test/test4.txt", O_RDONLY);
+	i = 1;
+	while (i < 39)
+	{
+		line = get_next_line(text4);
+		printf("line [%02d]: %s", i, line);
+		free(line);
+		i++;
+	}
+	close(text4); */
+	return (0);
+}
